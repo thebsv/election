842011892476ca2c3eb85db7ccde1e2ea060b2c1
Bhargav Srinivasan <bhargav.srinivasan92@gmail.com>
2024-08-11 04:37:42 -0400

    znode: applying socket performance optimizations

--- znetwork.py
+++ znetwork.py
@@ -55,100 +55,103 @@ from typing import List
 
 
 CONFIG_FILE = "server.config"
-
-# import concurrent.futures
-# class ZNode1:
-
-#     def __init__(self, send_timeout: int, so_linger: int, server_port: str) -> None:
-#         self.send_timeout = send_timeout
-#         self.so_linger = so_linger
-#         self.server_port = server_port
-
-#         self.server_socket = None
-#         self.client_sockets = {}
-        
-#         self.node_list = self._parse_configuration(CONFIG_FILE)
-#         print("servers: ", self.node_list)
-#         self._create_client_sockets(self.server_port, self.node_list)
-
-    
-#     def _parse_configuration(self, config_file: str) -> List[str]:
-#         servers = []
-#         with open(config_file, "r") as config:
-#             for line in config:
-#                 line = line.strip()
-#                 servers.append(line)
-#         return servers
+NO_QUEUE = 1
+SO_LINGER = 0
 
 
-#     def _create_client_sockets(self, server_port: str, node_list: List[str]) -> None:
-#         context = zmq.Context()
-#         for node in node_list:
-#             if node != server_port:
-#                 client_socket = context.socket(zmq.REQ)
-#                 client_socket.connect(f"tcp://localhost:{node}")
-#                 self.client_sockets[node] = client_socket
-    
-    
-#     def server_loop(self) -> None:
-#         context = zmq.Context()
-#         self.server_socket = context.socket(zmq.REP)
-#         self.server_socket.bind(f"tcp://*:{self.server_port}")
-#         print(f"Server started on port: {str(self.server_port)}")
-
-#         try:
-#             while True:
-#                 try:
-#                     message = self.server_socket.recv_string()
-#                     print(f"Received message: {message}")
-#                 except Exception as e:
-#                     print(f"Exception: {str(e)}")
-
-#                 try:
-#                     self.server_socket.send_string("ACK")
-#                 except Exception as e:
-#                     print(f"Exception: {str(e)}")
-#         except Exception as e:
-#             print(f"Exiting server... {str(e)}")
-    
-    
-#     def send_message(self, node: str, message: str) -> str:
-#         try:
-#             client_socket = self.client_sockets[node]
-#             client_socket.send(message.encode('utf-8'))
-#             return client_socket.recv_string()
-#         except Exception as e:
-#             print(f"Could not send message from {node}: {message}, {str(e)}")
-    
-
-# def main1():
-#     try:
-#         # Test server loop run 5252, 5253
-#         node1 = ZNode1(1, 1, "5252")
-#         node2 = ZNode1(1, 1, "5253")
-        
-#         with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-#             futures = { 
-#                 executor.submit(node1.server_loop) : node1, 
-#                 executor.submit(node2.server_loop): node2
-#             }
-
-#             # Test client socket, send a message to 5253
-#             rep = node1.send_message("5253", "hello")
-#             print(f"node1 to node2 reply: {str(rep)}")
-#             rep = node2.send_message("5252", "hello")
-#             print(f"node2 to node1 reply: {str(rep)}")
-
-#     except Exception as e:
-#         print(f"error starting server: {str(e)}")
-#         os._exit(1)
-
-
-class ZNode:
+"""
+import concurrent.futures
+class ZNode1:
 
     def __init__(self, send_timeout: int, so_linger: int, server_port: str) -> None:
         self.send_timeout = send_timeout
         self.so_linger = so_linger
+        self.server_port = server_port
+
+        self.server_socket = None
+        self.client_sockets = {}
+        
+        self.node_list = self._parse_configuration(CONFIG_FILE)
+        print("servers: ", self.node_list)
+        self._create_client_sockets(self.server_port, self.node_list)
+
+    
+    def _parse_configuration(self, config_file: str) -> List[str]:
+        servers = []
+        with open(config_file, "r") as config:
+            for line in config:
+                line = line.strip()
+                servers.append(line)
+        return servers
+
+
+    def _create_client_sockets(self, server_port: str, node_list: List[str]) -> None:
+        context = zmq.Context()
+        for node in node_list:
+            if node != server_port:
+                client_socket = context.socket(zmq.REQ)
+                client_socket.connect(f"tcp://localhost:{node}")
+                self.client_sockets[node] = client_socket
+    
+    
+    def server_loop(self) -> None:
+        context = zmq.Context()
+        self.server_socket = context.socket(zmq.REP)
+        self.server_socket.bind(f"tcp://*:{self.server_port}")
+        print(f"Server started on port: {str(self.server_port)}")
+
+        try:
+            while True:
+                try:
+                    message = self.server_socket.recv_string()
+                    print(f"Received message: {message}")
+                except Exception as e:
+                    print(f"Exception: {str(e)}")
+
+                try:
+                    self.server_socket.send_string("ACK")
+                except Exception as e:
+                    print(f"Exception: {str(e)}")
+        except Exception as e:
+            print(f"Exiting server... {str(e)}")
+    
+    
+    def send_message(self, node: str, message: str) -> str:
+        try:
+            client_socket = self.client_sockets[node]
+            client_socket.send(message.encode('utf-8'))
+            return client_socket.recv_string()
+        except Exception as e:
+            print(f"Could not send message from {node}: {message}, {str(e)}")
+    
+
+def main1():
+    try:
+        # Test server loop run 5252, 5253
+        node1 = ZNode1(1, 1, "5252")
+        node2 = ZNode1(1, 1, "5253")
+        
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            futures = { 
+                executor.submit(node1.server_loop) : node1, 
+                executor.submit(node2.server_loop): node2
+            }
+
+            # Test client socket, send a message to 5253
+            rep = node1.send_message("5253", "hello")
+            print(f"node1 to node2 reply: {str(rep)}")
+            rep = node2.send_message("5252", "hello")
+            print(f"node2 to node1 reply: {str(rep)}")
+
+    except Exception as e:
+        print(f"error starting server: {str(e)}")
+        os._exit(1)
+"""
+
+
+class ZNode:
+
+    def __init__(self, server_port: str) -> None:
         self.server_port = server_port
 
         self.server_socket = None
@@ -172,13 +175,32 @@ class ZNode:
         for node in node_list:
             if node != server_port:
                 client_socket = context.socket(zmq.REQ)
+                client_socket = self._apply_performance_optimizations(client_socket)
                 client_socket.connect(f"tcp://localhost:{node}")
                 self.client_sockets[node] = client_socket
     
+
+    def _apply_performance_optimizations(self, csocket):
+        # Socket performance optimizations
+            
+        # ZMQ related
+        csocket.setsockopt(zmq.IMMEDIATE, NO_QUEUE) # no queueing
+
+        # TCP related
+        csocket.setsockopt(zmq.TCP_KEEPALIVE, 1)
+        csocket.setsockopt(zmq.TCP_KEEPALIVE_IDLE, 60)
+        csocket.setsockopt(zmq.TCP_KEEPALIVE_CNT, 10)
+
+        # Other
+        csocket.setsockopt(zmq.LINGER, SO_LINGER) # disconnect immediately upon close
+
+        return csocket
+
     
     async def server_loop(self) -> None:
         context = zmq.asyncio.Context()
         self.server_socket = context.socket(zmq.REP)
+        self.server_socket = self._apply_performance_optimizations(self.server_socket)
         self.server_socket.bind(f"tcp://*:{self.server_port}")
         print(f"Server started on port: {str(self.server_port)}")
 
@@ -193,25 +215,35 @@ class ZNode:
                 try:
                     await self.server_socket.send_string("ACK")
                 except Exception as e:
-                    print(f"Exception: {str(e)}")
+                    print(f"Exception while sending from the server: {str(e)}")
         except Exception as e:
             print(f"Exiting server... {str(e)}")
+        finally:
+            self.server_socket.close()
     
     
     def send_message(self, node: str, message: str) -> str:
         try:
             client_socket = self.client_sockets[node]
-            client_socket.send(message.encode('utf-8'))
+            client_socket.send_string(message)
             return client_socket.recv_string()
         except Exception as e:
             print(f"Could not send message from {node}: {message}, {str(e)}")
+    
+
+    def __delattr__(self):
+        for client_sock in self.client_sockets:
+            try:
+                client_sock.close()
+            except Exception as e:
+                print(f"Error closing client socket: {str(e)}")
 
 
 async def main():
     try:
         # Test server loop run 5252, 5253
-        node1 = ZNode(1, 1, "5252")
-        node2 = ZNode(1, 1, "5253")
+        node1 = ZNode("5252")
+        node2 = ZNode("5253")
         
         t1 = asyncio.create_task(node1.server_loop()) 
         t2 = asyncio.create_task(node2.server_loop())
